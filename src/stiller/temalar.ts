15f3ab315538e807aad4e211755d18f4ba2a56ab
@reactkick <kad.rselcuk@gmail.com>
2025-06-08 02:20:56 +0300

    Create temalar.ts

---
+++ src/stiller/temalar.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const PrimaryButton = styled.button`
+  background-color: ${props => props.theme.colors.primary};
+  color: white;
+  padding: 10px 20px;
+`;
